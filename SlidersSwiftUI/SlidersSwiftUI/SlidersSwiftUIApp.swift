23af24e928474829453bfe474a92db2e56f6ee18
Vladislav <vladislavgalkin5@gmail.com>
2022-10-06 21:55:27 +0300

    A new project is created

---
+++ SlidersSwiftUI/SlidersSwiftUI/SlidersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersSwiftUIApp.swift
+//  SlidersSwiftUI
+//
+//  Created by Владислав on 6.10.22.
+//
+
+import SwiftUI
+
+@main
+struct SlidersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
